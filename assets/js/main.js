34dcc7c07a21d2312b6c1b88406bafb8f9359d8e
Ed Johnson-Williams <edjw@users.noreply.github.com>
2020-03-30 22:11:05 +0100

    more redesign; optimising js a bit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,31 @@
-if (window.netlifyIdentity) {
-	window.netlifyIdentity.on("init", user => {
-		if (!user) {
-			window.netlifyIdentity.on("login", () => {
-				document.location.href = "/admin/";
-			});
-		}
-	});
-}
-
 window.addEventListener("DOMContentLoaded", function () {
+	netlifyInit();
+	mobileMenu();
+});
+
+function mobileMenu() {
 	const navToggler = document.getElementById("nav_toggler");
 	const navList = document.getElementsByClassName("nav_list")[0];
-
 	navToggler.addEventListener("click", function () {
 		if (navToggler.innerText === "Menu") {
-
 			navList.classList.toggle("mobileShown");
 			navToggler.innerText = "Hide Menu";
 		}
-
 		else if (navToggler.innerText === "Hide Menu") {
 			navList.classList.toggle("mobileShown");
 			navToggler.innerText = "Menu";
 		}
-	})
+	});
+}
 
-
-
-});
+function netlifyInit() {
+	if (window.netlifyIdentity) {
+		window.netlifyIdentity.on("init", user => {
+			if (!user) {
+				window.netlifyIdentity.on("login", () => {
+					document.location.href = "/admin/";
+				});
+			}
+		});
+	}
+}
