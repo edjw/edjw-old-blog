3a25bd9295fedc6cc1505c28b65e0d46565def6b
Ed Johnson-Williams <edjw@users.noreply.github.com>
2020-04-29 12:51:11 +0100

    add alpine; rework navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,25 @@
 window.addEventListener("DOMContentLoaded", function () {
 	netlifyInit();
-	mobileMenu();
+	// mobileMenu();
 });
 
-function mobileMenu() {
-	const navToggler = document.getElementById("nav_toggler");
-	const navList = document.getElementsByClassName("nav_list")[0];
-	navToggler.addEventListener("click", function () {
-		if (navToggler.innerText === "Menu") {
-			navList.classList.toggle("mobileShown");
-			navToggler.innerText = "Hide Menu";
-			navToggler.setAttribute("aria-expanded", true)
-		}
-		else if (navToggler.innerText === "Hide Menu") {
-			navList.classList.toggle("mobileShown");
-			navToggler.innerText = "Menu";
-			navToggler.setAttribute("aria-expanded", false)
+// function mobileMenu() {
+// 	const navToggler = document.getElementById("nav_toggler");
+// 	const navList = document.getElementsByClassName("nav_list")[0];
+// 	navToggler.addEventListener("click", function () {
+// 		if (navToggler.innerText === "Menu") {
+// 			navList.classList.toggle("mobileShown");
+// 			navToggler.innerText = "Hide Menu";
+// 			navToggler.setAttribute("aria-expanded", true)
+// 		}
+// 		else if (navToggler.innerText === "Hide Menu") {
+// 			navList.classList.toggle("mobileShown");
+// 			navToggler.innerText = "Menu";
+// 			navToggler.setAttribute("aria-expanded", false)
 
-		}
-	});
-}
+// 		}
+// 	});
+// }
 
 function netlifyInit() {
 	if (window.netlifyIdentity) {
