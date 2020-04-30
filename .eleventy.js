90114ca7ed9d3e2cd847fb645cf625dd6dcfd09a
Ed Johnson-Williams <edjw@users.noreply.github.com>
2020-04-30 16:03:57 +0100

    reworking date and reading time section on posts a bit

--- .eleventy.js
+++ .eleventy.js
@@ -52,7 +52,7 @@ module.exports = function (eleventyConfig) {
 
 	// Date formatting (human readable)
 	eleventyConfig.addFilter("readableDate", dateObj => {
-		return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+		return DateTime.fromJSDate(dateObj).toFormat("dd MMMM yyyy");
 	});
 
 	// Date formatting (machine readable)
